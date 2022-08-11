3ccf87d55a068d4bfb9eded6104324453c9a0599
maycondsn <maycondsn@gmail.com>
2022-08-11 15:17:59 -0400

    add scoreboard

--- colors.py
+++ colors.py
@@ -1,5 +1,7 @@
 red = '\033[31m'
 green = '\033[32m'
 yellow = '\033[93m'
+blue = '\033[34m'
+orange = '\033[33m'
 
 reset = '\033[0m'
--- main.py
+++ main.py
@@ -3,6 +3,15 @@ import random
 import colors
 
 variables = ["rock", "paper", "scissors", "lizard", "spock"]
+score = [0, 0]
+
+rule = {
+    'rock': (['crushes', 'lizard'], ['crushes', 'scissors']),
+    'paper': (['disproves', 'spock'], ['covers', 'rock']),
+    'scissors': (['cuts', 'paper'],['decapites', 'lizard']),
+    'lizard': (['poisons', 'spock'], ['eats', 'paper']),
+    'spock': (['smashes', 'scissors'], ['vaporizes', 'rock'])
+}
 
 def Win(player1, player2, index):
     print(f'\n{player1} {rule[player1][index][0]} {player2}')
@@ -13,21 +22,18 @@ def Lose(player1, player2):
         print(f'\n{player2} {rule[player2][i][0]} {player1}') if(rule[player2][i][1] == player1) else ""
     print(f"{colors.red}You lose! :({colors.reset}\n")
 
-rule = {
-    'rock': (['crushes', 'lizard'], ['crushes', 'scissors']),
-    'paper': (['disproves', 'spock'], ['covers', 'rock']),
-    'scissors': (['cuts', 'paper'],['decapites', 'lizard']),
-    'lizard': (['poisons', 'spock'], ['eats', 'paper']),
-    'spock': (['smashes', 'scissors'], ['vaporizes', 'rock'])
-}
-
-def Running(player1, player2):
+def Running(player1, player2, score):
     x, index = 0, -1
     for i in range(2):
         if (rule[player1][i][1] == player2):
             x = 1
             index = i
-    Win(player1, player2, index) if(x == 1) else Lose(player1, player2)
+    if(x == 1):
+        Win(player1, player2, index)
+        score[0] += 1
+    else:
+        Lose(player1, player2)
+        score[1] += 1
     
 def Bot():
     return variables[random.randint(1, 5)-1]
@@ -37,13 +43,15 @@ def Game():
     
     while True:
         try:
+            print(f'{colors.blue}player: {score[0]}{colors.reset}   |   {colors.orange}bot: {score[1]}{colors.reset}\n')
             x = int(input("enter a value: ")) 
-            variable = variables[x-1] 
-            bot = Bot()     
+            variable = variables[x-1]
+            bot = Bot()
             print(f'You: {variable}') 
             print(f'Bot: {bot}')
-            print(f'\n{colors.yellow}DRAW!{colors.reset}\n') if variable == bot else Running(variable, bot)
+            print(f'\n{colors.yellow}DRAW!{colors.reset}\n') if variable == bot else Running(variable, bot, score)
             
         except:
-            print('Invalid value')
+            print('INVALID VALUE!')
+
 Game()
